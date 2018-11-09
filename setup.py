747c3735b84e62dec6968eff21f1b87ca83658e3
Sammy <31512872+ToxicSamN@users.noreply.github.com>
2018-11-09 08:59:35 -0700

    pypi package and rev up

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='pycrypt-samn',
-    version='1.2.2',
+    version='1.2.3',
     description='Customized Encryption module',
 	long_description=long_description,
     license='Apache',
