da5d15421bb8dee7a82208a072358065b4aa4caa
Sammy <31512872+ToxicSamN@users.noreply.github.com>
2018-12-06 11:21:28 -0700

    pip package upgrades in requirements

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='pycrypt-samn',
-    version='1.2.4',
+    version='1.2.6',
     description='Customized Encryption module',
 	long_description=long_description,
     license='Apache',
