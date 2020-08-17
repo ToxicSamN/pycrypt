dcf67c7eae6764f0892952e08d72c50e64dd243f
Sammy <sammy.d.shuck@gmail.com>
2020-08-17 15:40:32 -0600

    Updated pypi

--- pypi_pack_n_send.py
+++ pypi_pack_n_send.py
@@ -16,7 +16,7 @@ logger.addHandler(handler)
 print(os.getcwd())
 if not platform.platform().lower().find('windiows') == -1:
     venv_cmd = os.path.join(os.getcwd(), "venv\\Scripts\\activate")
-elif not platform.platform().lower().find('linux') == -1 or not platform.platform().lower().find('macos'):
+elif not platform.platform().lower().find('linux') == -1 or not platform.platform().lower().find('macos') == -1:
     venv_cmd = "source {}".format(os.path.join(os.getcwd(), "venv/bin/activate"))
 p = subprocess.Popen('{} && {} && python setup.py sdist bdist_wheel'.format(venv_cmd, "pip list"), shell=True, stderr=sys.stdout, stdout=sys.stdout)
 p.wait()
